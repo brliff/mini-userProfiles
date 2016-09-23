f7acd3fd0d2df6d1cdca109935b2ea20dc37eb3d
Brad.Lifferth <brad.lifferth@outlook.com>
2016-09-22 19:33:01 -0600

    complete mini project

---
+++ mainService.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Brad.Lifferth on 9/22/2016.
+ */
+angular.module('userProfiles').service('mainService', function() {
+   var data = [{
+            "id": 0,
+            "first_name": "george",
+            "last_name": "bluth",
+            "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
+        },
+        {
+            "id": 1,
+            "first_name": "lucille",
+            "last_name": "bluth",
+            "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
+        },
+        {
+            "id": 2,
+            "first_name": "oscar",
+            "last_name": "bluth",
+            "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
+        }];
+
+    this.getUsers = function() {
+        return data;
+    };
+
+    this.toggleFavorite = function(index) {
+        data[index].isFavorite = !data[index].isFavorite;
+        // if(!data[index].isFavorite){
+        //     data[index].isFavorite = true;
+        //     console.log([index] + ' SET TO TRUE');
+        // } else {
+        //     data[index].isFavorite = false;
+        //     console.log([index] + ' SET TO FALSE');
+        // }
+    };
+});
